c0ff7f62c999fd8f51f846f792bb666b0b4286c8
karen-99 <kardzavaryan@gmail.com>
2020-10-08 01:28:29 -0700

    feature

--- app/App.js
+++ app/App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Landing from './screens/Landing/Landing';
+import Loan from './screens/Landing/Loan';
+import Signup from './screens/Signup';
+import Signin from './screens/Signin';
+import Bank from './screens/Bank';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {useFonts} from 'expo-font';
+
+const Stack = createStackNavigator();
+
+const App: () => React$Node = () => {
+  let [fontsLoaded] = useFonts({
+    'OpenSans-BoldItalic': require('./assets/fonts/OpenSans-BoldItalic.ttf'),
+    'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'OpenSans-ExtraBold': require('./assets/fonts/OpenSans-ExtraBold.ttf'),
+    'OpenSans-ExtraBoldItalic': require('./assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
+    'OpenSans-Italic': require('./assets/fonts/OpenSans-Italic.ttf'),
+    'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
+    'OpenSans-LightItalic': require('./assets/fonts/OpenSans-LightItalic.ttf'),
+    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+    'OpenSans-SemiBoldItalic': require('./assets/fonts/OpenSans-SemiBoldItalic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <></>;
+  } else {
+    return (
+      <>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={Landing} options={{headerShown: false}}/>
+            <Stack.Screen name="Loan" component={Loan} options={{headerShown: false}}/>
+            <Stack.Screen name="Signin" component={Signin} options={{headerShown: false}}/>
+            <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}}/>
+            <Stack.Screen name="Bank" component={Bank} options={{headerShown: false}}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+    )
+  }
+};
+
+export default App;
